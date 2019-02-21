3982f89b49f8f62883c31f687191020c1ba9394e
christian <chrismoss411@gmail.com>
2019-02-21 23:21:43 +0900

    initial commit

--- Build/Intermediates.noindex/web3.build/Debug-iphoneos/web3.build/Objects-normal/arm64/browser.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/a/Documents/Appcelerator_Studio_Workspace/web3/web3/browser.m \
-  /Users/a/Documents/Appcelerator_Studio_Workspace/web3/Build/Intermediates.noindex/web3.build/Debug-iphoneos/web3.build/module.modulemap \
-  /Users/a/Documents/Appcelerator_Studio_Workspace/web3/web3/browser.h
--- Build/Intermediates.noindex/web3.build/Debug-iphoneos/web3.build/Objects-normal/armv7/web3_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/a/Documents/Appcelerator_Studio_Workspace/web3/Build/Intermediates.noindex/web3.build/Debug-iphoneos/web3.build/DerivedSources/web3_vers.c
